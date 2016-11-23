6a82b0386b7e3f2b997d1cf0116a13f88d4145e2
Me <Me@Tanvir>
2016-11-23 12:52:59 +0600

    first commit

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+ echo 'sajib';
+ 
+class Bangladesh{
+   public $name = '$name';
+   
+   public function Dhaka(){
+       echo 'Dhaka University';
+     
+       
+   }
+   public function Rangpur(){
+       echo 'Rangpur University';
+   }
+   
+}
+
+$bd = new Bangladesh();
+$bd->Dhaka();
